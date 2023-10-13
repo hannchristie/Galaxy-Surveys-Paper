3fda66fb90161774acfcf79d7dabccc6ba982699
Hannah Christie <hchris2@uwo.ca>
2023-10-12 22:17:00 -0400

    Create D_distribution.py

---
+++ D_distribution.py
@@ -0,0 +1,13 @@
+#%%
+# import packages
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import math as m
+import statistics as stats
+
+#%%
+
+# set theme
+sns.set_theme(style="whitegrid")
+
